fbc76ade5e43e10d1f18de78f1361b0d9f7cb4bb
QingmuDeng <qingmu.deng@students.olin.edu>
2018-03-04 18:42:53 -0500

    studio 6 latex

--- Studio6/svd_image.m
+++ Studio6/svd_image.m
@@ -1,9 +1,11 @@
 A = imread('blade-runner-2049-trailer-wallpapers-20-1920×811.jpg');
-A = rgb2gray(A);
+% A = rgb2gray(A);
 A = im2double(A);
+figure
+imshow(A)
 [U, S, V] = svd(A);
 V_t = V';
-approx = 200;
+approx = 10;
 compressed = U(:, 1:approx) * S(1:approx, 1:approx) * V_t(1:approx, :);
 figure
 imshow(compressed)
